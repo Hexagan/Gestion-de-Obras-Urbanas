6fd5e0e7c9d4669fccce9cee1477758bc269505c
Agustin Braco <bracoagustin@gmail.com>
2024-11-05 00:58:56 -0300

    [Dev] - Definición de campos para la base de datos, desarrollo de métodos e implementación de dos instancias completas

--- Sosa-Salvador-Braco-Pinzon/src/gestionar_obras.py
+++ Sosa-Salvador-Braco-Pinzon/src/gestionar_obras.py
@@ -1,8 +1,11 @@
 from abc import ABC, abstractmethod
-from .modelo_orm import db
+from typing import Optional
+from .modelo_orm import db, Obra
 import pandas as pd
 
+# Definir superclase abstracta
 class GestionarObra(ABC):
+  # Definir métodos abstractos
   @abstractmethod
   def extraer_datos(self, archivo: str) -> pd.DataFrame:
     pass
@@ -11,35 +14,63 @@ class GestionarObra(ABC):
   def conectar_db(self) -> bool:
     pass
   
-  # @abstractmethod
-  # def mapear_orm(self):
-  #   pass
+  @abstractmethod
+  def mapear_orm(self) -> bool:
+    pass
   
-  # @abstractmethod
-  # def limpiar_datos(self):
-  #   pass
+  @abstractmethod
+  def limpiar_datos(self) -> pd.DataFrame:
+    pass
   
-  # @abstractmethod
-  # def cargar_datos(self):
-  #   pass
+  @abstractmethod
+  def cargar_datos(self) -> bool:
+    pass
   
-  # @abstractmethod
-  # def nueva_obra(self):
-  #   pass
+  @abstractmethod
+  def nueva_obra(self) -> Optional[Obra]:
+    pass
   
-  # @abstractmethod
-  # def obtener_indicadores(self):
-  #   pass
+  @abstractmethod
+  def obtener_indicadores(self) -> dict:
+    pass
 
+# Implementar subclase
 class GestionarObraCSV(GestionarObra):
+  # Campos requeridos (definidos previamente)
+  __REQUERIDO = [
+      "nombre",
+      "tipo",
+      "area",
+      "barrio",
+      "comuna",
+      "monto_contrato",
+      "etapa",
+      "contratacion_tipo",
+      "nro_contratacion",
+      "licitacion_oferta_empresa",
+      "expediente_numero",
+      "destacada",
+      "fecha_inicio",
+      "fecha_fin_inicial",
+      "financiamiento",
+      "porcentaje_avance",
+      "plazo_meses",
+      "mano_obra",
+  ]
+
+  def __init__(self):
+    self.datos = None
+
   def __str__(self) -> str:
     return 'Gestor de obras iniciado correctamente'
 
+# Implementar métodos
   def extraer_datos(self, archivo: str) -> pd.DataFrame:
     try:
-      datos = pd.read_csv(archivo, delimiter=';')
+      self.datos = pd.read_csv(archivo, delimiter=';')
+
       print('Datos extraídos con éxito')
-      return datos
+      return self.datos
     except Exception as error:
       print(f'Error al extraer datos: {error}')
       return pd.DataFrame()
@@ -47,8 +78,112 @@ class GestionarObraCSV(GestionarObra):
   def conectar_db(self) -> bool:
     try:
       db.connect()
+
       print('Base de datos conectada exitosamente')
       return True
     except Exception as error:
       print('Error al conectarse con la base de datos:', error)
       return False
+
+  def mapear_orm(self) -> bool:
+    try:
+      db.create_tables([Obra], safe=True)
+      
+      print('Tablas creadas exitosamente')
+      return True
+    except Exception as error:
+      print('Error al mapear ORM:', error)
+      return False
+
+  def limpiar_datos(self, datos: pd.DataFrame) -> pd.DataFrame:
+    try:
+      REQUERIDO = self.__REQUERIDO
+      
+      # Renombrar columna y campos por compatibilidad 
+      datos = datos.rename(columns={'expediente-numero': 'expediente_numero'})
+      datos['destacada'] = datos['destacada'].apply(lambda x: True if x == 'SI' else False)
+
+      # Filtrar y limpiar datos
+      datos_filtrados = datos.dropna(how='all')
+      datos_limpios = datos_filtrados.dropna(subset=REQUERIDO)
+      self.datos = datos_limpios
+
+      print('Datos limpios generados con éxito')
+      return self.datos
+    except Exception as error:
+      print(f'Error al limpiar datos: {error}')
+      return datos
+    
+  def cargar_datos(self, datos: pd.DataFrame) -> bool:
+    try:
+      REQUERIDO = self.__REQUERIDO
+
+      # Iterar cada fila para crear entradas sin duplicados
+      for _, row in datos.iterrows():
+        if Obra.select().where(Obra.nombre == row['nombre']).exists():
+          print(f"La obra '{row['nombre']}' ya existe.")
+          continue
+
+        if all(key in row for key in REQUERIDO):
+          Obra.create(
+            nombre = row['nombre'],
+            tipo = row['tipo'],
+            area = row['area'],
+            barrio = row['barrio'],
+            comuna = row['comuna'],
+            monto_contrato = row['monto_contrato'],
+            etapa = row['etapa'],
+            contratacion_tipo = row['contratacion_tipo'],
+            nro_contratacion = row['nro_contratacion'],
+            licitacion_oferta_empresa = row['licitacion_oferta_empresa'],
+            expediente_numero = row['expediente_numero'],
+            destacada = row['destacada'],
+            fecha_inicio = row['fecha_inicio'],
+            fecha_fin_inicial = row['fecha_fin_inicial'],
+            financiamiento = row['financiamiento'],
+            porcentaje_avance = row['porcentaje_avance'],
+            plazo_meses = row['plazo_meses'],
+            mano_obra = row['mano_obra']
+          )
+
+      print('Datos cargados exitosamente')
+      return True
+    except Exception as error:
+      print(f"Error al cargar los datos: {error}")
+      return False
+
+  def nueva_obra(self) -> Optional[Obra]:
+    try:
+      # Solicitar datos al usuario
+      nombre = input("Ingrese el nombre de la obra: ")
+      tipo = input("Ingrese el tipo de obra: ")
+      area = input("Ingrese el área responsable: ")
+      barrio = input("Ingrese el barrio: ")
+      comuna = int(input('Ingrese la comuna: '))
+      monto_contrato = float(input("Ingrese el monto del contrato: "))
+
+      # Crear nueva entrada con esos datos
+      nueva_obra = Obra(
+        nombre = nombre,
+        tipo = tipo,
+        area = area,
+        barrio = barrio,
+        comuna = comuna,
+        monto_contrato = monto_contrato
+      )
+      nueva_obra.save()
+
+      print('Obra creada exitosamente')
+      return nueva_obra
+    except Exception as error:
+      print(f'Error al crear nueva obra: {error}')
+      return None
+
+  def obtener_indicadores(self) -> dict:
+    try:
+      indicadores = {}
+      return indicadores
+
+    except Exception as error:
+      print('Error al obtener indicadores:', error)
+      return {}
--- Sosa-Salvador-Braco-Pinzon/src/modelo_orm.py
+++ Sosa-Salvador-Braco-Pinzon/src/modelo_orm.py
@@ -10,29 +10,66 @@ class BaseModel(Model):
     database = db
 
 class Obra(BaseModel):
-  def nuevo_proyecto(self):
-    pass
+  # Definir campos
+  nombre = CharField(unique=True)
+  tipo = CharField()
+  area = CharField()
+  barrio = CharField()
+  comuna = IntegerField(default=0)
+  monto_contrato = DecimalField()
+  etapa = CharField(null=True)
+  contratacion_tipo = CharField(null=True)
+  nro_contratacion = CharField(null=True)
+  licitacion_oferta_empresa = CharField(null=True)
+  expediente_numero = CharField(null=True)
+  destacada = BooleanField(default=False)
+  fecha_inicio = DateField(null=True)
+  fecha_fin_inicial = DateField(null=True)
+  financiamiento = CharField(null=True)
+  porcentaje_avance = IntegerField(default=0)
+  plazo_meses = IntegerField(default=0)
+  mano_obra = IntegerField(default=0)
 
-  def iniciar_contratacion(self):
-    pass
+  # Establecer campos
+  def nuevo_proyecto(self):
+    self.etapa = 'Proyecto'
+    self.save()
+
+  def iniciar_contratacion(self, contratacion_tipo, nro_contratacion):
+    self.contratacion_tipo = contratacion_tipo
+    self.nro_contratacion = nro_contratacion
+    self.save()
   
-  def adjudicar_obra(self):
-    pass
+  def adjudicar_obra(self, licitacion_oferta_empresa, expediente_numero):
+    self.licitacion_oferta_empresa = licitacion_oferta_empresa
+    self.expediente_numero = expediente_numero
+    self.save()
   
-  def iniciar_obra(self):
-    pass
+  def iniciar_obra(self, destacada, fecha_inicio, fecha_fin_inicial, financiamiento, mano_obra):
+    self.destacada = destacada
+    self.fecha_inicio = fecha_inicio
+    self.fecha_fin_inicial = fecha_fin_inicial
+    self.financiamiento = financiamiento
+    self.mano_obra = mano_obra
+    self.save()
   
-  def actualizar_porcentaje_avance(self):
-    pass
+  def actualizar_porcentaje_avance(self, porcentaje):
+    self.porcentaje_avance = porcentaje
+    self.save()
   
-  def incrementar_plazo(self):
-    pass
+  def incrementar_plazo(self, meses):
+    self.plazo_meses += meses
+    self.save()
   
-  def incrementar_mano_obra(self):
-    pass
+  def incrementar_mano_obra(self, cantidad):
+    self.mano_obra += cantidad
+    self.save()
   
   def finalizar_obra(self):
-    pass
+    self.etapa = 'Finalizada'
+    self.porcentaje_avance = 100
+    self.save()
   
   def rescindir_obra(self):
-    pass
+    self.etapa = 'Rescindida'
+    self.save()
